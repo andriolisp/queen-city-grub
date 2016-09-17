f07b533821d694820ca99f27d0ccdd8c2b1d7dc1
Chris Philips <cphilips@redventures.com>
2016-09-16 22:33:11 -0400

    More cleaning, don't need RestaurantsService test folder

--- test/SearchService/test.js
+++ test/SearchService/test.js
@@ -1,17 +1,15 @@
-var SearchService = require("../../src/SearchService")
+var SearchService = require('../../src/SearchService')
 
 var request = {
-    "message" : "...",
-    "defaultLocation" : ["35.2270869","-80.8431267"],
-    "entities" : {
-      "foodType" : "pizza"
-    }
-};
+  'message': '...',
+  'defaultLocation': ['35.2270869', '-80.8431267'],
+  'entities': {
+    'foodType': 'pizza'
+  }
+}
 
-var searchPromise = SearchService.find(request);
+var searchPromise = SearchService.find(request)
 
 searchPromise.then(function (restaurants) {
-
-  console.log("Search for South End Pizza", restaurants);
-
-});
+  console.log('Search for South End Pizza', restaurants)
+})
