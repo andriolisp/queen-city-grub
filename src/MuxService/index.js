db16ff94140f3caf4dcbc84c5d36b283d8da5c1a
Thierry Kimenyi <tkimenyi@redventures.com>
2016-09-17 10:44:51 -0400

    mux service done, we decided to not have a merger

--- src/MuxService/index.js
+++ src/MuxService/index.js
@@ -1,16 +1,26 @@
 var proberService = require('../../src/ProberService')
-var mergerService = require('../../src/MergerService')
-
+var classifierService = require('../../src/ClassifierService')
+var entitiesService = require('../../src/EntitiesService')
+var searchService = require('../../src/SearchService')
 var MuxService = {}
 
+// this calls the classifier and determines if we should call the prober
+// or if we have enough information to search for restaurants
 MuxService.handleRequest = function (request) {
   return new Promise(function (resolve) {
     if (request['monteCarlo']) {
       proberService.getUserSuggestions(request).then(resolve)
     } else {
-      mergerService.handleRequest(request).then(resolve)
+      classifierService.classify(request).then(function (request) {
+        if (request.classify.recommend) {
+          proberService.getUserSuggestions(request).then(resolve)
+        } else {
+          entitiesService.tagEntitities(request).then(function (request) {
+            searchService.find(request).then(resolve)
+          })
+        }
+      })
     }
   })
 }
-
 module.exports = MuxService
