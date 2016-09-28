3039f7a6ddb02b7a39f6e8d264c22308f056dc40
Carlos <candrioli@redventures.com>
2016-09-28 17:26:48 -0300

    Test

--- src/Application/MuxController.js
+++ src/Application/MuxController.js
@@ -4,224 +4,226 @@ var MuxService = require('../MuxService');
 var MuxController = {};
 
 var quickReplies = [{
-    "content_type":"text",
-    "title":"Sim",
-    "payload":"2"
-},{
-    "content_type":"text",
-    "title":"Não",
-    "payload":"0"
-},{
-    "content_type":"text",
-    "title":"Talvez",
-    "payload":"1"
-}];
+  "content_type": "text",
+  "title": "Sim",
+  "payload": "2"
+}, {
+    "content_type": "text",
+    "title": "Não",
+    "payload": "0"
+  }, {
+    "content_type": "text",
+    "title": "Talvez",
+    "payload": "1"
+  }];
 
-var colors = ["blue","green","yellow"];
-var labels = ["A","B","C"];
+var colors = ["blue", "green", "yellow"];
+var labels = ["A", "B", "C"];
 
 var replyMessageText = function (res) {
 
-    var recipientId = res.userId;
+  var recipientId = res.userId;
 
-    if (res.isDone) {
+  if (res.isDone) {
 
-        var elements = [];
-    
-        res.restaurants.forEach(function (restaurant, i) {
-        
-            var buttons = [];
-            
-            if (restaurant.phoneNumber != null) {
-                buttons.push({
-                    "type": "phone_number",
-                    "payload": restaurant.phoneNumber.replace(/\s/g, ''),
-                    "title": "Ligar"
-                })
-            }
+    var elements = [];
 
-            if (restaurant.websiteLink != null) {
-                buttons.push({
-                    "type" : "web_url",
-                    "url" : restaurant.websiteLink,
-                    "title" : "Ir para o site"
-                })
-            }
-            
-            var elementData = {
-                "title": restaurant.name,
-                "subtitle": "",
-                "item_url": restaurant.bubbleLink,
-                "image_url": "https://maps.googleapis.com/maps/api/staticmap?center="+restaurant.location.join(",")+"&zoom=16&size=500x270&maptype=roadmap&markers=color:"+colors[i]+"|label:"+labels[i]+"|"+restaurant.location.join(","),
-                "buttons": buttons
-            }
+    res.restaurants.forEach(function (restaurant, i) {
 
-            if(restaurant.rating) {
-              elementData.subtitle = restaurant.rating + " pontos"
-            }
+      var buttons = [];
 
-            elements.push(elementData);
-            
-        });
+      if (restaurant.phoneNumber != null) {
+        buttons.push({
+          "type": "phone_number",
+          "payload": restaurant.phoneNumber.replace(/\s/g, ''),
+          "title": "Ligar"
+        })
+      }
 
-        if (elements.length > 0) {
+      if (restaurant.websiteLink != null) {
+        buttons.push({
+          "type": "web_url",
+          "url": restaurant.websiteLink,
+          "title": "Ir para o site"
+        })
+      }
 
-           // Let the user know we found some suggestions 
-           MessengerController.sendText(recipientId, "Calma ai, acho que eu tenho algumas idéias para você...").then(function () {
+      var elementData = {
+        "title": restaurant.name,
+        "subtitle": "",
+        "item_url": restaurant.bubbleLink,
+        "image_url": "https://maps.googleapis.com/maps/api/staticmap?center=" + restaurant.location.join(",") + "&zoom=16&size=500x270&maptype=roadmap&markers=color:" + colors[i] + "|label:" + labels[i] + "|" + restaurant.location.join(","),
+        "buttons": buttons
+      }
 
-                // Send a carosel with the suggested restaurants
-                MessengerController.sendAttachment(recipientId, {  
-                    'type': 'template',
-                    'payload': {  
-                        'template_type': 'generic',
-                        'elements': elements
-                    }
-                }).catch(console.log);
-                
-           });
-            
-        } else {
-            
-            // There are no suggestions to present
-            MessengerController.sendText(recipientId, "Putzzz! Não consegui achar nada que eu ache que você vai gostar!").catch(console.log);
-            
-        }
+      if (restaurant.rating) {
+        elementData.subtitle = restaurant.rating + " pontos"
+      }
+
+      elements.push(elementData);
+
+    });
+
+    if (elements.length > 0) {
+
+      // Let the user know we found some suggestions 
+      MessengerController.sendText(recipientId, "Calma ai, acho que eu tenho algumas idéias para você...").then(function () {
+
+        // Send a carosel with the suggested restaurants
+        MessengerController.sendAttachment(recipientId, {
+          'type': 'template',
+          'payload': {
+            'template_type': 'generic',
+            'elements': elements
+          }
+        }).catch(console.log);
+
+      });
 
     } else {
 
-        // Send the question along with some quick replies
-        MessengerController.sendQuickReplyText(recipientId, res.question, quickReplies).catch(console.log);
+      // There are no suggestions to present
+      MessengerController.sendText(recipientId, "Putzzz! Não consegui achar nada que eu ache que você vai gostar!").catch(console.log);
 
     }
+
+  } else {
+
+    // Send the question along with some quick replies
+    MessengerController.sendQuickReplyText(recipientId, res.question, quickReplies).catch(console.log);
+
+  }
 
 };
 
 var MessengerController = {};
 
 MuxController.setMessengerController = function (messengerController) {
-    MessengerController = messengerController;
+  MessengerController = messengerController;
 }
 
 MuxController.handleLocation = function (senderId, recipientId, location) {
 
-    MessengerController.sendText(senderId, "Legal!! Agora eu consigo achar algo no maximo a 3km de você!!").then(function () {
-        cache.put('location-'+recipientId, location, 600000);
-    });
+  MessengerController.sendText(senderId, "Legal!! Agora eu consigo achar algo no maximo a 3km de você!!").then(function () {
+    cache.put('location-' + recipientId, location, 600000);
+  });
 
 };
 
 MuxController.handleMessageText = function (senderId, messageText, shortCircuitMonteCarlo) {
-    var defaultLocation = cache.get('location-'+senderId) || ['-23.598088', '-46.683349'];
+  var defaultLocation = cache.get('location-' + senderId) || ['-23.598088', '-46.683349'];
 
-    if (shortCircuitMonteCarlo) {
+  if (shortCircuitMonteCarlo) {
 
-        MuxService.handleRequest({
-            "defaultLocation" : defaultLocation,
-            "response" : messageText,
-            "userId" : senderId,
-            "monteCarlo" : true
-        }).then(replyMessageText).catch(console.log);
-        
-    } else {
+    MuxService.handleRequest({
+      "defaultLocation": defaultLocation,
+      "response": messageText,
+      "userId": senderId,
+      "monteCarlo": true
+    }).then(replyMessageText).catch(console.log);
 
-        MuxService.handleRequest({
-            "defaultLocation" : defaultLocation,
-            "message" : messageText,
-            "userId" : senderId,
-            "monteCarlo" : false
-        }).then(replyMessageText).catch(console.log);
+  } else {
 
-    }
+    MuxService.handleRequest({
+      "defaultLocation": defaultLocation,
+      "message": messageText,
+      "userId": senderId,
+      "monteCarlo": false
+    }).then(replyMessageText).catch(console.log);
+
+  }
 
 };
 
-MuxController.handleHelpMenu = function(senderId, recipientId, payload) {
-  if(senderId && recipientId) {
+MuxController.handleHelpMenu = function (senderId, recipientId, payload) {
+  if (senderId && recipientId) {
     console.log('[MuxController.handleHelpMenu] senderId: ', senderId)
     console.log('[MuxController.handleHelpMenu] recipientId: ', recipientId)
     console.log('[MuxController.handleHelpMenu] payload: ', payload)
     var defaultLocation = ['-23.598088', '-46.683349'];
-    var location = cache.get('location-'+recipientId)
-    if(location !== null && location != undefined) {
+    var location = cache.get('location-' + recipientId)
+    if (location !== null && location != undefined) {
       MessengerController.sendText(senderId, "Vamos lá! Já sei aonde você tá, então eu vou te fazer algumas perguntas antes de te sugerir algo, ok?").then(function () {
         MuxController.handleMessageText(senderId, null, true);
       }).catch(console.log);
     } else {
-      MuxController.handleMessageText({
-            "defaultLocation" : defaultLocation,
-            "message" : "Opa!! vamos te ajudar a achar um lugar legal para comer ai perto, você pode me mandar sua localização?",
-            "userId" : senderId,
-            "monteCarlo" : true,
-            "askLocation": true
-      }).then(replyMessageText).catch(console.log)
+      MessengerController.sendText(senderId, "Opa!! vamos te ajudar a achar um lugar legal para comer ai perto, você pode me mandar sua localização?").then(function () {
+        MuxService.handleRequest({
+          "defaultLocation": defaultLocation,
+          "message": payload,
+          "userId": senderId,
+          "monteCarlo": true,
+          "askLocation": true
+        }).then(replyMessageText).catch(console.log)
+      }).catch(console.log)
     }
   }
 }
 
 MuxController.handleMainMenu = function (senderId) {
 
-    // Send the main menu
-    MessengerController.sendAttachment(senderId, {
-        "type": "template",
-        "payload": {
-            "template_type": "generic",
-            "elements": [{
-                "title": "Bem vindo ao Sampa Grub",
-                "subtitle": "Eu posso te ajudar a encontrar aonde comer!",
-                "image_url": "https://sampa-grub.herokuapp.com/bemvindo.jpg" ,
-                "buttons": [{
-                    "type": "web_url",
-                    "title": "Pagina no Facebook",
-                    "url": "https://www.facebook.com/sampagrub/",
-                }]
-            },{
-                "title": "Não sei",
-                "subtitle": "Me ajuda a achar algum lugar legal?",
-                "image_url": "https://sampa-grub.herokuapp.com/nao-sei.jpg",
-                "buttons": [{
-                    "type": "postback",
-                    "title": "Me ajuda",
-                    "payload": "HELP_MENU"
-                }]
-            },{
-                "title": "Mexicana",
-                "subtitle": "Ai ai ai!!! Nada como tacos!",
-                "image_url": "https://sampa-grub.herokuapp.com/mexicana.jpg",
-                "buttons": [{
-                    "type": "postback",
-                    "title": "Comer mexicana!",
-                    "payload": "mexicana"
-                }]
-            },{
-                "title": "Italiano",
-                "subtitle": "Mamma Mia! Uma Lasanha!",
-                "image_url": "https://sampa-grub.herokuapp.com/italiana.jpg",
-                "buttons": [{
-                    "type": "postback",
-                    "title": "Comida italiana!",
-                    "payload": "italiana"
-                }]
-            },{
-                "title": "Americana",
-                "subtitle": "Hmmm... Buffalo wings!",
-                "image_url": "https://sampa-grub.herokuapp.com/americana.jpg",
-                "buttons": [{
-                    "type": "postback",
-                    "title": "Comida americana!",
-                    "payload": "americana"
-                }]
-            },{
-                "title": "Chinesa",
-                "subtitle": "Nada como yakisoba agora!",
-                "image_url": "https://sampa-grub.herokuapp.com/chinesa.jpg",
-                "buttons": [{
-                    "type": "postback",
-                    "title": "Comida chinesa!",
-                    "payload": "LOCATION_TEST"
-                }]
-            }]
-        }
-    }).catch(console.log);
+  // Send the main menu
+  MessengerController.sendAttachment(senderId, {
+    "type": "template",
+    "payload": {
+      "template_type": "generic",
+      "elements": [{
+        "title": "Bem vindo ao Sampa Grub",
+        "subtitle": "Eu posso te ajudar a encontrar aonde comer!",
+        "image_url": "https://sampa-grub.herokuapp.com/bemvindo.jpg",
+        "buttons": [{
+          "type": "web_url",
+          "title": "Pagina no Facebook",
+          "url": "https://www.facebook.com/sampagrub/",
+        }]
+      }, {
+          "title": "Não sei",
+          "subtitle": "Me ajuda a achar algum lugar legal?",
+          "image_url": "https://sampa-grub.herokuapp.com/nao-sei.jpg",
+          "buttons": [{
+            "type": "postback",
+            "title": "Me ajuda",
+            "payload": "HELP_MENU"
+          }]
+        }, {
+          "title": "Mexicana",
+          "subtitle": "Ai ai ai!!! Nada como tacos!",
+          "image_url": "https://sampa-grub.herokuapp.com/mexicana.jpg",
+          "buttons": [{
+            "type": "postback",
+            "title": "Comer mexicana!",
+            "payload": "mexicana"
+          }]
+        }, {
+          "title": "Italiano",
+          "subtitle": "Mamma Mia! Uma Lasanha!",
+          "image_url": "https://sampa-grub.herokuapp.com/italiana.jpg",
+          "buttons": [{
+            "type": "postback",
+            "title": "Comida italiana!",
+            "payload": "italiana"
+          }]
+        }, {
+          "title": "Americana",
+          "subtitle": "Hmmm... Buffalo wings!",
+          "image_url": "https://sampa-grub.herokuapp.com/americana.jpg",
+          "buttons": [{
+            "type": "postback",
+            "title": "Comida americana!",
+            "payload": "americana"
+          }]
+        }, {
+          "title": "Chinesa",
+          "subtitle": "Nada como yakisoba agora!",
+          "image_url": "https://sampa-grub.herokuapp.com/chinesa.jpg",
+          "buttons": [{
+            "type": "postback",
+            "title": "Comida chinesa!",
+            "payload": "LOCATION_TEST"
+          }]
+        }]
+    }
+  }).catch(console.log);
 
 }
 
